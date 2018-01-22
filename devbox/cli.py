98c32f74f419a8d45da74d004b3bd10c78aa890f
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-01-22 20:57:45 +0000

    Docopt setup

--- devbox/cli.py
+++ devbox/cli.py
@@ -1,2 +1,13 @@
+"""
+Devbox is a python command line application that assists in provisioning a development server.
+
+Usage:
+    devbox
+
+Options:
+    -h --help
+"""
+from docopt import docopt
+
 def main():
-    print('Hello World')
+    arguments = docopt(__doc__)
