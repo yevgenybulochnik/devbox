d253588155607bce69d018b8abdb4c876a5f0675
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-01-24 23:01:30 +0000

    Flake8 syntax adjustment

--- install.py
+++ install.py
@@ -11,12 +11,15 @@ LOG_DIR = p.join(SCRIPT_DIR, 'logs')
 os.makedirs(SETUP_DIR, exist_ok=True)
 os.makedirs(LOG_DIR, exist_ok=True)
 
+
 class dv_command:
     stamps = []
+
     def __init__(self, cmd_name, path=SCRIPT_DIR, type='sh'):
-        self.cmd= cmd_name
+        self.cmd = cmd_name
         self.path = path
         self.type = type
+
     def run(self):
         self.time_logger(self.cmd)
         if self.type == 'py':
@@ -31,6 +34,7 @@ class dv_command:
                 stderr=log
             )
             proc.wait()
+
     def logger(self, command):
         command = command.split()
         if command[0] == 'sudo':
@@ -42,6 +46,7 @@ class dv_command:
         log.write('\n' + spacer + command + spacer + '\n')
         log.flush()
         return log
+
     def time_logger(self, command):
         command = command.split()
         time_stamp = datetime.datetime.now()
@@ -52,16 +57,15 @@ class dv_command:
             command = command[0]
         spacer = '\n' + ('-' * 40) + '\n'
         log = open(p.join(LOG_DIR, 'dv_init_time.log'), 'a')
-        log.write(spacer + command + '\n----' +  time_stamp_str + '\n')
+        log.write(spacer + command + '\n----' + time_stamp_str + '\n')
         log.flush()
         self.stamps.append(time_stamp)
-
 
 
 commands = [
     dv_command('wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz -N', SETUP_DIR),
     dv_command('tar -xf Python-3.6.3.tgz', SETUP_DIR),
-    dv_command(p.join('.', SETUP_DIR,'Python-3.6.3', 'configure'), SETUP_DIR),
+    dv_command(p.join('.', SETUP_DIR, 'Python-3.6.3', 'configure'), SETUP_DIR),
     dv_command('rm -rf ' + SETUP_DIR),
     dv_command('print("Finish")', type='py')
 ]
