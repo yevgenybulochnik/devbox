59ee744fe03619cf786a50c505337a5b1fed1ee1
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-02-07 04:20:35 +0000

    Create virtual host files

--- devbox/local/a2config.py
+++ devbox/local/a2config.py
@@ -3,6 +3,8 @@ import sys
 import os
 import os.path as p
 from plumbum import local
+import pkg_resources
+from string import Template
 
 
 class a2config(Base_Command):
@@ -26,7 +28,29 @@ class a2config(Base_Command):
         for mod in mods:
             a2enmod(mod)
 
+    def create_virtual_hosts(self):
+        vhs = [
+            'jupyter',
+            'preview',
+            'static',
+            'webterm'
+        ]
+        server_name = self.options['--domain']
+        for vh_template in vhs:
+            resource = pkg_resources.resource_filename('devbox', f'config_files/a2/{vh_template}')
+            temp = Template(open(resource).read())
+            vh = temp.substitute({'DOMAIN': server_name})
+            if vh_template == 'static':
+                conf = open(f'/etc/apache2/sites-available/{server_name}.conf', 'w')
+                conf.write(vh)
+                conf.close()
+            else:
+                conf = open(f'/etc/apache2/sites-available/{vh_template}.{server_name}.conf', 'w')
+                conf.write(vh)
+                conf.close()
+
     def run(self):
         if not os.geteuid() == 0:
             sys.exit('\n This command must be run as Root \n')
         self.check_install()
+        self.create_virtual_hosts()
