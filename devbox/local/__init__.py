b91494e2d4aaa9817ca34dff74b71df5a0a07c6f
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-02-12 18:30:33 +0000

    Base setup for setup command

--- devbox/local/__init__.py
+++ devbox/local/__init__.py
@@ -1,2 +1,3 @@
 from .adduser import AddUser as adduser
 from .a2config import A2Config as a2config
+from .setup import Setup as setup
---
+++ devbox/local/setup.py
@@ -0,0 +1,13 @@
+from devbox.base_command import BaseCommand
+
+
+class Setup(BaseCommand):
+    """
+    Usage:
+        setup
+
+    Options:
+        -h, --help
+    """
+    def run(self):
+        print("hello world")
